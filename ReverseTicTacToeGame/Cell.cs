5744205c5455e6744ae58832a24b3886161f0193
tahelka <73593127+tahelka@users.noreply.github.com>
2023-05-19 18:24:12 +0300

    uploading project

---
+++ ReverseTicTacToeGame/Cell.cs
@@ -0,0 +1,22 @@
+﻿namespace game
+{
+    internal class Cell
+    {
+        private const char k_Empty = ' ';
+        internal char Symbol { get; set; }
+        internal int XDimension { get; set; }
+        internal int YDimension { get; set; }
+
+        internal Cell(int i_XDimension, int i_YDimension)
+        {
+            Symbol = k_Empty;
+            XDimension = i_XDimension;
+            YDimension = i_YDimension;
+        }
+
+        internal bool IsCellEmpty()
+        {
+            return Symbol == k_Empty;
+        }
+    }
+}
---
+++ ReverseTicTacToeGame/GameManager.cs
@@ -0,0 +1,84 @@
+﻿namespace game
+{
+    internal class GameManager
+    {
+        private GameLogic m_GameCore;
+        private ConsoleIO m_UserInterface;
+
+        internal GameManager()
+        {
+            m_UserInterface = new ConsoleIO();
+            int numOfPlayers = m_UserInterface.WelcomeUserAndGetNumOfPlayers();
+            Player player1 = m_UserInterface.GetPlayerDetailsFromUser('X');
+            Player player2;
+
+            if (numOfPlayers == 1)
+            {
+                player2 = new Player("Computer", 'O')
+                {
+                    IsComputer = true
+                };
+            }
+            else
+            {
+                player2 = m_UserInterface.GetPlayerDetailsFromUser('O');
+            }
+
+            int boardSize = m_UserInterface.GetBoardSizeFromPlayer();
+            m_GameCore = new GameLogic(player1, player2, boardSize);
+        }
+
+        internal void RunGame()
+        {
+            do
+            {
+                m_GameCore.SetGameForNewRound();
+                while (!m_GameCore.IsRoundOver)
+                {
+                    playTurn();
+                }
+
+                m_UserInterface.DisplayTheFinalBoardAndSummary(m_GameCore);
+            }
+            while (m_UserInterface.DoesPlayerWantToPlayAnotherRound());
+        }
+
+        private void playTurn()
+        {
+            if (m_GameCore.CurrentPlayer.IsComputer)
+            {
+                m_GameCore.ApplyComputerPlayerTurn();
+            }
+            else
+            {
+                m_UserInterface.DisplayBoard(m_GameCore.Board);
+                m_UserInterface.DisplayWhoseTurn(m_GameCore.CurrentPlayer.Name, m_GameCore.CurrentPlayer.Symbol);
+                applyHumanPlayerTurn();
+            }
+        }
+
+        private void applyHumanPlayerTurn()
+        {
+            bool isMoveApplied = false;
+
+            do
+            {
+                bool doesPlayerWantToQuit = m_UserInterface.GetMoveFromPlayer(m_GameCore.Board.BoardSize, out int row, out int col);
+                if (doesPlayerWantToQuit)
+                {
+                    m_GameCore.PrepareGameForQuitting();
+                }
+                else if (!m_GameCore.Board.IsCellOnBoardEmpty(row - 1, col - 1))
+                {
+                    m_UserInterface.DisplayCellIsOccupiedMsg();
+                }
+                else
+                {
+                    m_GameCore.ApplyMove(row - 1, col - 1);
+                    isMoveApplied = true;
+                }
+            } 
+            while (!isMoveApplied && !m_GameCore.IsRoundOver);
+        }
+    }
+}
